12bb1246afd5a36dae9708321605ebdf76f12ee4
kcuellar <katherine.cuellar93@gmail.com>
2018-07-03 22:40:44 -0400

    format folder structure

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+//initialize Firebase
+var config = {
+    apiKey: "AIzaSyCsEOSz-2hEjIE1O4b_quHWEf2S7t0lqlI",
+    authDomain: "train-schedule-89ecf.firebaseapp.com",
+    databaseURL: "https://train-schedule-89ecf.firebaseio.com",
+    projectId: "train-schedule-89ecf",
+    storageBucket: "train-schedule-89ecf.appspot.com",
+    messagingSenderId: "754830123111"
+  };
+  firebase.initializeApp(config);
